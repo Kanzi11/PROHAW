56d597013e648c0a48c48de58c16a46a5fbfe368
LuffiP <molinaguillermo506@gmail.com>
2023-05-25 08:18:36 -0600

    Cantidad y existencias
    Validacion y mostrar existencias

--- controllers/public/detail.js
+++ controllers/public/detail.js
@@ -16,12 +16,15 @@ document.addEventListener('DOMContentLoaded', async ()=>{
     const JSON = await dataFetch(PRODUCTO_API, 'readOne', FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if(JSON.status){
+        console.log(JSON);
          // Se colocan los datos en la página web de acuerdo con el producto seleccionado previamente.
         document.getElementById('imagen').src = SERVER_URL.concat('img/productos/', JSON.dataset.imagen_producto);
         document.getElementById('nombre').textContent = JSON.dataset.nombre_producto;
         document.getElementById('descripcion').textContent = JSON.dataset.detalle_producto;
         document.getElementById('precio').textContent = JSON.dataset.precio_producto;
         document.getElementById('id_producto').value = JSON.dataset.id_producto;
+        document.getElementById('existencia').value = JSON.dataset.existencias;
+        document.getElementById('cantidad').setAttribute("max",JSON.dataset.existencias     )
     }else{
         // Se presenta un mensaje de error cuando no existen datos para mostrar.
         document.getElementById('title').textContent = JSON.exception;
